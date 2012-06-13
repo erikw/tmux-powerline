dc507fc418d240932fb7e5800eb694cc10674c03
Erik Westrup <erik.westrup@gmail.com>
2012-06-13 11:27:45 +0200

    Read MPD connection info from environtment variables.

--- addins/mpd_np.c
+++ addins/mpd_np.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <mpd/client.h>
 
 #ifdef DEBUG
@@ -10,20 +11,108 @@
 #endif
 #define DEBUG_PRINTF(...) do{ if (DO_DEBUG) { printf(__VA_ARGS__);} } while(0)
 
-/* Output the current song if MPD is in a playing state. */
+/* Output the current song if MPD is in a playing state. The connection host is
+ * specified like for mpc with an environment variable.*/
 int main(int argc, const char *argv[])
 {
-	struct mpd_connection *mpd_connection = mpd_connection_new("localhost", 6600, 1000);
+	char *mpd_host = NULL;
+	char *mpd_password = NULL;
+	unsigned int mpd_port = 0;
+
+	char *mpd_host_m = NULL;
+	char *mpd_password_m = NULL;
+
+	const char *mpd_host_str = getenv("MPD_HOST");
+	if (mpd_host_str == NULL || strlen(mpd_host_str) == 0) {
+		DEBUG_PRINTF("No envvar MPD_HOST set or empty. Using default value (localhost).\n");
+		mpd_host = "localhost";
+	} else {
+		size_t mpd_host_len = strlen(mpd_host_str);
+		char mpd_host_env[mpd_host_len + 1];
+		strncpy(mpd_host_env, mpd_host_str, mpd_host_len);
+		mpd_host_env[mpd_host_len] = '\0';
+
+		char *pch = strtok(mpd_host_env, "@");
+		char *first = NULL;
+		char *second = NULL;
+		if (pch != NULL) {
+			first = pch;
+		}
+		pch = strtok(NULL, "@");
+		if (pch != NULL) {
+			second = pch;
+		}
+
+		if (first != NULL && second != NULL) {
+			DEBUG_PRINTF("%s - %s\n", first, second);
+			size_t first_len = strlen(first);
+			size_t second_len = strlen(second);
+			mpd_password_m = (char *) malloc(first_len + 1);
+			mpd_host_m= (char *) malloc(second_len + 1);
+			if (mpd_password_m == NULL || mpd_host_m == NULL) {
+				DEBUG_PRINTF("Failed alloc password/host.\n");
+				return EXIT_FAILURE;
+			}
+			strncpy(mpd_password_m, first, first_len);
+			mpd_password_m[first_len] = '\0';
+			strncpy(mpd_host_m, second, second_len);
+			mpd_host_m[second_len] = '\0';
+		} else  if (first != NULL) {
+			DEBUG_PRINTF("%s\n", first);
+			size_t first_len = strlen(first);
+			mpd_host_m = (char *) malloc(first_len + 1);
+			if (mpd_host_m == NULL ) {
+				DEBUG_PRINTF("Failed alloc host.\n");
+				return EXIT_FAILURE;
+			}
+			strncpy(mpd_host_m, first, first_len);
+			mpd_host_m[first_len] = '\0';
+		}
+	}
+
+	if (mpd_host_m != NULL) {
+		mpd_host =  mpd_host_m;
+	}
+
+	if (mpd_password_m != NULL) {
+		mpd_password =  mpd_password_m;
+	}
+
+	const char *mpd_port_env = getenv("MPD_PORT");
+	if (mpd_port_env == NULL || strlen(mpd_port_env) == 0) {
+		DEBUG_PRINTF("No envvar MPD_PORT set or empty. Using default value (6600).\n");
+		mpd_port = 6600;
+	} else {
+		int mpd_port_m = atoi(mpd_port_env);
+		if (mpd_port_m == 0) {
+			DEBUG_PRINTF("Could not convert MPD_PORT to int.\n");
+			return EXIT_FAILURE;
+		} else if (mpd_port_m < 0) {
+			DEBUG_PRINTF("Negative port?!\n");
+			return EXIT_FAILURE;
+		} else {
+			mpd_port = mpd_port_m;
+			DEBUG_PRINTF("Using port %i\n", mpd_port);
+		}
+	}
+
+
+	struct mpd_connection *mpd_connection = mpd_connection_new(mpd_host, mpd_port, 1000);
 	if (mpd_connection == NULL) {
 		DEBUG_PRINTF("%s\n", "Could Not connect");
 		return EXIT_FAILURE;
 	}
 
-	bool authenticated = mpd_run_password(mpd_connection, "Vmb8s2snakmJJ6aBNk7N5GXd5XWXHrFv");
-	if (!authenticated) {
-		DEBUG_PRINTF("Failed to authenticate.\n");
-		return EXIT_FAILURE;
+	if (mpd_password != NULL) {
+		bool authenticated = mpd_run_password(mpd_connection, mpd_password);
+		if (!authenticated) {
+			DEBUG_PRINTF("Failed to authenticate.\n");
+			return EXIT_FAILURE;
+		}
 	}
+
+	free(mpd_host_m);
+	free(mpd_password_m);
 
 	bool sent_status = mpd_send_status(mpd_connection);
 	if (!sent_status) {
