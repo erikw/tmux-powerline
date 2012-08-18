3868b8c391d29bf88b0a793cc9cc43d5b7ef8b68
Gino Lucero <glucero@gmail.com>
2012-08-18 15:05:37 -0600

    support for linux? (all i have is ubuntu to test it on)

--- segments/cpu.sh
+++ segments/cpu.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 # Prints the CPU usage: user% sys% idle
 
-top -l1 | grep "CPU usage" | awk '{ print $3, $5, $7 }'
+if [ $PLATFORM == 'mac' ]; then
+  top -l 1 | grep "CPU usage" | awk '{ print $3, $5, $7 }'
+elif [ $PLATFORM == 'linux' ]; then
+  top -n 1 | grep "Cpu(s)" | awk '{ print $2, $3, $5 }' | sed 's/\w\{2\},//g'
+fi
 
 exit 0
