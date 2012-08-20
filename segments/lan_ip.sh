bfa683279cbdc6546bf5dec2933f188806b38bdf
Erik Westrup <erik.westrup@gmail.com>
2012-08-21 00:18:51 +0200

    Wrap the changes in an if clause because I have (well I'm on my iMac do but refuse to use) an OS X installation to test on.

--- segments/lan_ip.sh
+++ segments/lan_ip.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 # Prints the local network IP address for a staticly defined NIC or search for an IP address on all active NICs.
 
-#nic=eth0		# Use this NIC.
-nic="USE_FIRST_FOUND"	# Find the first IP address on all active NICs.
-
-if [ "$nic" == "USE_FIRST_FOUND" ]; then
-	all_nics=$(ifconfig | cut -d ' ' -f1)
-	nics=(${all_nics[@]//lo/}) 	# Remove lo interface.
-
-	for nic in ${nics[@]}; do
-		lan_ip=$(ifconfig "$nic" |  grep -Po "(?<=inet addr:)[^ ]+")
-		[ -n "$lan_ip" ] && break
-	done
+# TODO fix the mac part so it also can search for interfaces like the Linux one can.
+if [ "$PLATFORM" == "mac" ]; then
+	nic0="en0"
+	nic1="en1"
+	ip0=$(/sbin/ifconfig ${nic0} 2>/dev/null | grep 'inet ')
+	ip1=$(/sbin/ifconfig ${nic1} 2>/dev/null | grep 'inet ')
 else
-	lan_ip=$(ifconfig "$nic" | grep -Po "(?<=inet addr:)[^ ]+")
+	#nic=eth0		# Use this NIC.
+	nic="USE_FIRST_FOUND"	# Find the first IP address on all active NICs.
+
+	if [ "$nic" == "USE_FIRST_FOUND" ]; then
+		all_nics=$(ifconfig | cut -d ' ' -f1)
+		nics=(${all_nics[@]//lo/}) 	# Remove lo interface.
+
+		for nic in ${nics[@]}; do
+			lan_ip=$(ifconfig "$nic" |  grep -Po "(?<=inet addr:)[^ ]+")
+			[ -n "$lan_ip" ] && break
+		done
+	else
+		lan_ip=$(ifconfig "$nic" | grep -Po "(?<=inet addr:)[^ ]+")
+	fi
 fi
 
 if [ -n "$lan_ip" ]; then
