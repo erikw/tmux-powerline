48ac9f13d1671cbddfdb4c3bca087313c20dd956
Derek Ashley Thomas <derekathomas@gmail.com>
2012-10-26 16:02:57 +0900

    Add segments that give greater detail about vcs
    Note: In the current implementation, I have only added git support.
    This is because I generally do not deal with svn and hg repositories
    at the moment and do not know the best way to implement them. In some
    cases, these segments may be meaningless for certain vcs's (i.e.
    staged/prepared files may not be meaningful in svn). A description is
    given below
    
    * new file:   segments/vcs_compare.sh
        This compares the current branch with that of it's remote branch (as
        reached with "git fetch"). It may look like: ↓ 3 ↑ 2
    * new file:   segments/vcs_modified.sh
        This shows the number of items that are modified and not staged
    * new file:   segments/vcs_others.sh
        This shows the number of items that are new / un-tracked by the vcs
    * new file:   segments/vcs_staged.sh
        These are items that are staged / prepared to be commited into the vcs
    * modified:   status-left.sh
        These are set to on by default

--- status-left.sh
+++ status-left.sh
@@ -54,6 +54,34 @@ vcs_branch+=(["background"]="colour29")
 vcs_branch+=(["separator"]="${separator_right_bold}")
 register_segment "vcs_branch"
 
+declare -A vcs_compare
+vcs_compare+=(["script"]="${segments_path}/vcs_compare.sh")
+vcs_compare+=(["foreground"]="white")
+vcs_compare+=(["background"]="colour60")
+vcs_compare+=(["separator"]="${separator_right_bold}")
+register_segment "vcs_compare"
+
+declare -A vcs_staged
+vcs_staged+=(["script"]="${segments_path}/vcs_staged.sh")
+vcs_staged+=(["foreground"]="white")
+vcs_staged+=(["background"]="colour64")
+vcs_staged+=(["separator"]="${separator_right_bold}")
+register_segment "vcs_staged"
+
+declare -A vcs_modified
+vcs_modified+=(["script"]="${segments_path}/vcs_modified.sh")
+vcs_modified+=(["foreground"]="white")
+vcs_modified+=(["background"]="red")
+vcs_modified+=(["separator"]="${separator_right_bold}")
+register_segment "vcs_modified"
+
+declare -A vcs_others
+vcs_others+=(["script"]="${segments_path}/vcs_others.sh")
+vcs_others+=(["foreground"]="black")
+vcs_others+=(["background"]="colour245")
+vcs_others+=(["separator"]="${separator_right_bold}")
+register_segment "vcs_others"
+
 # Print the status line in the order of registration above.
 print_status_line_left
 
