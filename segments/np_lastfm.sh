24bcb54d70013fb0748c2e0b4b22ac2dcfa8b862
Travis Thompson <butters08@gmail.com>
2012-11-26 22:43:55 -0800

    np_lastfm.sh: now with caching :D

--- segments/np_lastfm.sh
+++ segments/np_lastfm.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 USERNAME=lastfm_username
-wget -qO- http://ws.audioscrobbler.com/1.0/user/$USERNAME/recenttracks.txt|head -n 1|sed -e 's/^[0-9]*,//'|sed 's/\xe2\x80\x93/-/'
+UPDATE_INTERVAL=30	# In seconds
+
+###############
+
+if [ ! -d /tmp/tmux-powerline ]; then
+	mkdir -p /tmp/tmux-powerline
+fi
+
+if [ ! -f /tmp/tmux-powerline/lastfm.txt ]; then
+	LAST_UPDATE=0
+else
+	LAST_UPDATED=$(stat -c %X /tmp/tmux-powerline/lastfm.txt)
+fi
+
+INTERVAL_AGO=$(date --date="$UPDATE_INTERVAL second ago" +%s)
+
+if [ $LAST_UPDATED -le $INTERVAL_AGO ]; then
+	touch /tmp/tmux-powerline/lastfm.txt
+	wget -qO- http://ws.audioscrobbler.com/1.0/user/$USERNAME/recenttracks.txt|head -n 1|sed -e 's/^[0-9]*,//'|sed 's/\xe2\x80\x93/-/' | tee /tmp/tmux-powerline/lastfm.txt
+else
+	cat /tmp/tmux-powerline/lastfm.txt
+fi
