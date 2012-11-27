e9b608a6bb2d3608ac1073d5df9e31a09a29a099
Travis Thompson <butters08@gmail.com>
2012-11-26 22:49:21 -0800

    Fixed some bugs

--- segments/np_lastfm.sh
+++ segments/np_lastfm.sh
@@ -9,7 +9,7 @@ if [ ! -d /tmp/tmux-powerline ]; then
 fi
 
 if [ ! -f /tmp/tmux-powerline/lastfm.txt ]; then
-	LAST_UPDATE=0
+	LAST_UPDATED=0
 else
 	LAST_UPDATED=$(stat -c %X /tmp/tmux-powerline/lastfm.txt)
 fi
@@ -18,7 +18,8 @@ INTERVAL_AGO=$(date --date="$UPDATE_INTERVAL second ago" +%s)
 
 if [ $LAST_UPDATED -le $INTERVAL_AGO ]; then
 	touch /tmp/tmux-powerline/lastfm.txt
-	wget -qO- http://ws.audioscrobbler.com/1.0/user/$USERNAME/recenttracks.txt|head -n 1|sed -e 's/^[0-9]*,//'|sed 's/\xe2\x80\x93/-/' | tee /tmp/tmux-powerline/lastfm.txt
+	wget -qO- http://ws.audioscrobbler.com/1.0/user/$USERNAME/recenttracks.txt|head -n 1|sed -e 's/^[0-9]*,//'|sed 's/\xe2\x80\x93/-/' > /tmp/tmux-powerline/lastfm.txt
+	cat /tmp/tmux-powerline/lastfm.txt
 else
 	cat /tmp/tmux-powerline/lastfm.txt
 fi
