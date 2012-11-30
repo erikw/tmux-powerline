1d85659747e295f44617cdc628cd38788b9a33f9
Jeff Felchner <accounts+git@thekompanee.com>
2012-11-30 01:16:53 -0600

    Remove a comment that is out of sync with the 'left' statusbar file

--- status-right.sh
+++ status-right.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# This script prints a string will be evaluated for text attributes (but not shell commands) by tmux. It consists of a bunch of segments that are simple shell scripts/programs that output the information to show. For each segment the desired foreground and background color can be specified as well as what separator to use. The script the glues together these segments dynamically so that if one script suddenly does not output anything (= nothing should be shown) the separator colors will be nicely handled.
 
 TMUX_POWERLINE_HOME=$(dirname $0)
 
