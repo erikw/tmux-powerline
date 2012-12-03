c2c0f16c686ef373336acc02005d0e90bb92c466
Jeff Felchner <accounts+git@thekompanee.com>
2012-12-02 21:08:59 -0600

    Now that duplication has been removed, only one script file is necessary

--- powerline.sh
+++ powerline.sh
@@ -12,6 +12,19 @@ export TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
   "vcs_others 245 0" \
 )
 
+export TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
+  "pwd 234 255" \
+  "mail_count 33 255" \
+  "np_mpd 234 37" \
+  "cpu 24 255" \
+  "load 29 255" \
+  "battery 60 255" \
+  "weather_yahoo 37 255" \
+  "date_day 9 255" \
+  "date_full 37 255" \
+  "time 235 255" \
+)
+
 export TMUX_POWERLINE_HOME=$(dirname $0)
 
 source "$TMUX_POWERLINE_HOME/config/default.sh"
@@ -23,9 +36,9 @@ source "$TMUX_POWERLINE_HOME/lib/formatting.sh"
 source "$TMUX_POWERLINE_HOME/lib/powerline.sh"
 
 # Mute this statusbar?
-mute_status_check "left"
+mute_status_check $1
 
 # Print the status line in the order of registration above.
-print_powerline "left"
+print_powerline $1
 
 exit 0
