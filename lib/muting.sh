2f45623a42f8ebb551105dd045ba74684b2eafef
Jeff Felchner <accounts+git@thekompanee.com>
2012-12-02 21:35:27 -0600

    Extract the common logic for the mute file into its own method

--- lib/muting.sh
+++ lib/muting.sh
@@ -1,19 +1,22 @@
+###
+# Muting Logic
+#
+# In all cases $1 is the side to be muted (eg left/right)
+#
 muted() {
-  local side=$1
-  local tmux_session=$(tmux display -p "#S")
-  local mute_file="${TMUX_POWERLINE_TEMPORARY_DIRECTORY}/mute_${tmux_session}_${side}"
-
-  [ -e "$mute_file" ];
+  [ -e "$(__mute_file $1)" ];
 }
 
-mute_status() {
-  local side=$1
-  local tmux_session=$(tmux display -p "#S")
-  local mute_file="${TMUX_POWERLINE_TEMPORARY_DIRECTORY}/mute_${tmux_session}_${side}"
-
-  if [ -e  "$mute_file" ]; then
-    rm "$mute_file"
+toggle_mute_status() {
+  if muted $1; then
+    rm "$(__mute_file $1)"
   else
-    touch "$mute_file"
+    touch "$(__mute_file $1)"
   fi
+}
+
+__mute_file() {
+  local tmux_session=$(tmux display -p "#S")
+
+  echo -n "${TMUX_POWERLINE_TEMPORARY_DIRECTORY}/mute_${tmux_session}_$1"
 }
