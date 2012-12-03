a3eff411c6f411b2f266a9e6081f5f355c95d3db
Jeff Felchner <accounts+git@thekompanee.com>
2012-12-03 00:58:38 -0600

    Refactor so that the segment contents are in their own array and therefore immune to spacing issues

--- lib/powerline.sh
+++ lib/powerline.sh
@@ -4,22 +4,22 @@ print_powerline() {
   local side="$1"
   eval "local input_segments=(\"\${TMUX_POWERLINE_${side^^}_STATUS_SEGMENTS[@]}\")"
   local powerline_segments=()
+  local powerline_segment_contents=()
 
   __process_segment_defaults
   __process_scripts
   __process_colors
 
-  for powerline_segment in "${powerline_segments[@]}"; do
-    local powerline_segment=($powerline_segment)
+  for segment_index in "${!powerline_segments[@]}"; do
+    local powerline_segment=(${powerline_segments[$segment_index]})
 
     local background_color=${powerline_segment[1]}
     local foreground_color=${powerline_segment[2]}
     local separator=${powerline_segment[3]}
-    local content=${powerline_segment[4]}
-    local separator_background_color=${powerline_segment[5]}
-    local separator_foreground_color=${powerline_segment[6]}
+    local separator_background_color=${powerline_segment[4]}
+    local separator_foreground_color=${powerline_segment[5]}
 
-    eval "__print_${side}_segment ${content} ${background_color} ${foreground_color} ${separator} ${separator_background_color} ${separator_foreground_color}"
+    eval "__print_${side}_segment ${segment_index} ${background_color} ${foreground_color} ${separator} ${separator_background_color} ${separator_foreground_color}"
   done
 }
 
@@ -45,9 +45,7 @@ __process_scripts() {
     local script="$TMUX_POWERLINE_SEGMENTS_HOME/${powerline_segment[0]}.sh"
     local output=$($script)
 
-    powerline_segment[4]=$output
-
-    powerline_segments[$segment_index]="${powerline_segment[@]}"
+    powerline_segment_contents[$segment_index]=$output
   done
 }
 
@@ -57,12 +55,12 @@ __process_colors() {
     local next_segment=(${powerline_segments[segment_index + 1]})
 
     if [ $side == 'left' ]; then
-      powerline_segment[5]=${next_segment[1]:-$TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR}
+      powerline_segment[4]=${next_segment[1]:-$TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR}
     elif [ $side == 'right' ]; then
-      powerline_segment[5]=${previous_background_color:-$TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR}
+      powerline_segment[4]=${previous_background_color:-$TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR}
     fi
 
-    powerline_segment[6]=${powerline_segment[1]}
+    powerline_segment[5]=${powerline_segment[1]}
 
     local previous_background_color=${powerline_segment[1]}
 
@@ -71,19 +69,19 @@ __process_colors() {
 }
 
 __print_left_segment() {
-  local content=$1
+  local content=${powerline_segment_contents[$1]}
   local content_background_color=$2
   local content_foreground_color=$3
   local separator=$4
   local separator_background_color=$5
   local separator_foreground_color=$6
 
-  __print_colored_content $content $content_background_color $content_foreground_color
+  __print_colored_content "$content" $content_background_color $content_foreground_color
   __print_colored_content $separator $separator_background_color $separator_foreground_color
 }
 
 __print_right_segment() {
-  local content=$1
+  local content=${powerline_segment_contents[$1]}
   local content_background_color=$2
   local content_foreground_color=$3
   local separator=$4
@@ -91,5 +89,5 @@ __print_right_segment() {
   local separator_foreground_color=$6
 
   __print_colored_content $separator $separator_background_color $separator_foreground_color
-  __print_colored_content $content $content_background_color $content_foreground_color
+  __print_colored_content "$content" $content_background_color $content_foreground_color
 }
