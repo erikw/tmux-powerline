f83b74ea4774763ad1bfffd91fe7214bf5783ee3
Erik Westrup <erik.westrup@gmail.com>
2012-12-14 15:45:30 -0800

    Too many tabs here.

--- config/default.sh
+++ config/default.sh
@@ -1,9 +1,9 @@
 # Default configuration.
 
-debug_mode_enabled	() {
+debug_mode_enabled() {
 	[ -n "$TMUX_POWERLINE_DEBUG_MODE_ENABLED" -a "$TMUX_POWERLINE_DEBUG_MODE_ENABLED" != "false" ];
 }
 
-patched_font_in_use () {
+patched_font_in_use() {
 	[ -z "$TMUX_POWERLINE_PATCHED_FONT_IN_USE" -o "$TMUX_POWERLINE_PATCHED_FONT_IN_USE" != "false" ];
 }
