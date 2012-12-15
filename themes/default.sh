43fff3c61692e62d1636da237323b38c0bd5128e
Erik Westrup <erik.westrup@gmail.com>
2012-12-14 17:53:05 -0800

    Modified default theme to look like before.

--- themes/default.sh
+++ themes/default.sh
@@ -22,6 +22,8 @@ if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
 		"tmux_session_info 148 234" \
 		"hostname 33 0" \
+		#"ifstat 30 255" \
+		#"ifstat_sys 30 255 \
 		"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
 		"wan_ip 24 255" \
 		"vcs_branch 29 88" \
@@ -37,10 +39,12 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 		"pwd 89 211" \
 		"mail_count_maildir 9 255" \
 		"np_mpd 234 37" \
-		"cpu 240 136" \
+		#"cpu 240 136" \
 		"load 237 167" \
+		#"tmux_mem_cpu_load 234 136" \
 		"battery 137 127" \
 		"weather_yahoo 37 255" \
+		"xkb_layout 125 117" \
 		"date_day 235 136" \
 		"date_full 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 		"time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
