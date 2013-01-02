98f05c6a31bf7c5ea8f9626855f80b67328f807d
Erik Westrup <erik.westrup@gmail.com>
2013-01-01 19:40:26 -0800

    Format options for hostname segment.

--- segments/hostname.sh
+++ segments/hostname.sh
@@ -1,6 +1,27 @@
 # Prints the hostname.
 
+TMUX_POWERLINE_SEG_HOSTNAME_FORMAT_DEFAULT="short"
+
+generate_segmentrc() {
+	read -d '' rccontents  << EORC
+# Use short or long format for the hostname. Can be {"short", "long"}
+export TMUX_POWERLINE_SEG_HOSTNAME_FORMAT="${TMUX_POWERLINE_SEG_HOSTNAME_FORMAT_DEFAULT}"
+EORC
+	echo "$rccontents"
+}
+
+__process_settings() {
+	if [ -z "$TMUX_POWERLINE_SEG_HOSTNAME_FORMAT" ]; then
+		export TMUX_POWERLINE_SEG_HOSTNAME_FORMAT="${TMUX_POWERLINE_SEG_HOSTNAME_FORMAT_DEFAULT}"
+	fi
+}
 run_segment() {
-	hostname -s
+	__process_settings
+	local opts=""
+	if [ "$TMUX_POWERLINE_SEG_HOSTNAME_FORMAT" == "short" ]; then
+		opts="--short"
+	fi
+
+	hostname ${opts}
 	return 0
 }
