5de94936be50a5d8d1de6f607d1baa6734db4cd7
Derek Thomas <derekathomas@gmail.com>
2013-01-09 10:48:02 +0900

    Add earthquake information segment
    This segment currently only looks at the 'goo' earthquake rss feed,
    which takes it's information from the Japan Meteorological Agency.
    This segment can be customized to display recent earthquakes that occur
    in a given 'alert-window' above a certain magnitude threshhold. This
    information should not and cannot be used as an early warning system,
    but is helpful in alerting you to large quakes that may have happened
    nearby.

--- themes/default.sh
+++ themes/default.sh
@@ -39,6 +39,7 @@ fi
 
 if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
+		#"earthquake 3 0" \
 		"pwd 89 211" \
 		"mailcount 9 255" \
 		"now_playing 234 37" \
