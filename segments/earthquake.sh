70cac3f9c387e446cc69fb9ca780d81894c4e52d
Filip Chabik <hadret@gmail.com>
2013-01-14 15:40:13 +0100

    Stat same as shell_is_osx does for BSD.

--- segments/earthquake.sh
+++ segments/earthquake.sh
@@ -72,7 +72,7 @@ __goo_earthquake() {
 	magnitude_number=""
 	timestamp=""
 	if [[ -f "$tmp_file" ]]; then
-		if shell_is_osx; then
+		if shell_is_osx || shell_is_bsd; then
 			last_update=$(stat -f "%m" ${tmp_file})
 		elif shell_is_linux; then
 			last_update=$(stat -c "%Y" ${tmp_file})
