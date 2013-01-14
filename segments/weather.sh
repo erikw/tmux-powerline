0bbb534d4f42b930532ad4f74314667473192b85
Derek Thomas <derekathomas@gmail.com>
2013-01-14 15:30:58 +0900

    Add 'snow/windy' to weather segment

--- segments/weather.sh
+++ segments/weather.sh
@@ -119,7 +119,7 @@ __get_condition_symbol() {
         	#echo "☂"
         	echo "☔"
         	;;
-    	"snow" | "mixed snow and sleet" | "snow flurries" | "light snow showers" | "blowing snow" | "sleet" | "hail" | "heavy snow" | "scattered snow showers" | "snow showers" | "light snow")
+    	"snow" | "mixed snow and sleet" | "snow flurries" | "light snow showers" | "blowing snow" | "sleet" | "hail" | "heavy snow" | "scattered snow showers" | "snow showers" | "light snow" | "snow/windy" )
         	#echo "☃"
         	echo "❅"
         	;;
