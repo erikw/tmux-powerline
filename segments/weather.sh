9be5189302926e2cd8394eb316522f0b63e02e14
Erik Jansson <erikjansson90@gmail.com>
2013-02-04 13:16:18 +0100

    Add "rain and snow" to weather segment.

--- segments/weather.sh
+++ segments/weather.sh
@@ -122,7 +122,7 @@ __get_condition_symbol() {
             	echo "☼"
         	fi
         	;;
-    	"rain" | "mixed rain and snow" | "mixed rain and sleet" | "freezing drizzle" | "drizzle" | "light drizzle" | "freezing rain" | "showers" | "mixed rain and hail" | "scattered showers" | "isolated thundershowers" | "thundershowers" | "light rain with thunder" | "light rain")
+    	"rain" | "mixed rain and snow" | "mixed rain and sleet" | "freezing drizzle" | "drizzle" | "light drizzle" | "freezing rain" | "showers" | "mixed rain and hail" | "scattered showers" | "isolated thundershowers" | "thundershowers" | "light rain with thunder" | "light rain" | "rain and snow")
         	#echo "☂"
         	echo "☔"
         	;;
