9b122df76ecbd1b01f251574c7b1ac8494fd087f
Erik Jansson <erikjansson90@gmail.com>
2013-02-04 23:06:11 +0100

    Possible workaround for issue #133. Shouldn't break anything else (I think).

--- lib/powerline.sh
+++ lib/powerline.sh
@@ -68,7 +68,7 @@ __process_colors() {
 	for segment_index in "${!powerline_segments[@]}"; do
 		local powerline_segment=(${powerline_segments[$segment_index]})
 	 	# Find the next segment that produces content (i.e. skip empty segments).
-		for next_segment_index in $(seq $(($segment_index + 1)) ${#powerline_segments}) ; do
+		for next_segment_index in $(eval echo {$(($segment_index + 1))..${#powerline_segments}}) ; do
 			[[ -n ${powerline_segments[next_segment_index]} ]] && break
 		done
 		local next_segment=(${powerline_segments[$next_segment_index]})
