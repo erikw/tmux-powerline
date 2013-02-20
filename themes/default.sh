a600b773e15ed5191357654c4da14e32d80fa57c
Alexander Luberg <alex@luberg.me>
2013-02-21 00:02:29 +0400

    my setup

--- themes/default.sh
+++ themes/default.sh
@@ -23,12 +23,12 @@ TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SE
 
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
-		"tmux_session_info 148 234" \
+		#"tmux_session_info 148 234" \
 		"hostname 33 0" \
 		#"ifstat 30 255" \
 		#"ifstat_sys 30 255" \
-		"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
-		"wan_ip 24 255" \
+		#"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
+		#"wan_ip 24 255" \
 		"vcs_branch 29 88" \
 		"vcs_compare 60 255" \
 		"vcs_staged 64 255" \
@@ -40,11 +40,11 @@ fi
 if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
 		#"earthquake 3 0" \
-		"pwd 89 211" \
-		"mailcount 9 255" \
+		#"pwd 89 211" \
+		#"mailcount 9 255" \
 		"now_playing 234 37" \
 		#"cpu 240 136" \
-		"load 237 167" \
+		#"load 237 167" \
 		#"tmux_mem_cpu_load 234 136" \
 		"battery 137 127" \
 		"weather 37 255" \
