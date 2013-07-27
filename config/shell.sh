2cfc64d96209a49d5d3f36f98934b31aa78c6d3d
YOSHIDA Hiroki <hyoshida@appirits.com>
2013-07-27 16:15:52 +0900

    Fixed how to check the OS type.

--- config/shell.sh
+++ config/shell.sh
@@ -1,9 +1,11 @@
 # Shell Configuration
 # vi: sw=8 ts=8 noet
 
+ostype() { echo $OSTYPE | tr '[A-Z]' '[a-z]'; }
+
 export SHELL_PLATFORM='unknown'
 
-case "$OSTYPE" in
+case "$(ostype)" in
 	*'linux'*	) SHELL_PLATFORM='linux'	;;
 	*'darwin'*	) SHELL_PLATFORM='osx'		;;
 	*'bsd'*		) SHELL_PLATFORM='bsd'		;;
