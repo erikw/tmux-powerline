c5c60e2c83cf5335bc970cb32ddbb9df79026d19
Erik Westrup <erik.westrup@gmail.com>
2015-02-21 12:06:01 +0100

    OSX solution for getting gmail count works for linux too.
    My theory is that the output from the gmail query is now on one line,
    ans was multiple lines before.

--- segments/mailcount.sh
+++ segments/mailcount.sh
@@ -140,11 +140,7 @@ __count_gmail() {
 
     	# Hide password from command line (visible with e.g. ps(1)).
     	echo -e "user=${TMUX_POWERLINE_SEG_MAILCOUNT_GMAIL_USERNAME}@${TMUX_POWERLINE_SEG_MAILCOUNT_GMAIL_SERVER}\npassword=${TMUX_POWERLINE_SEG_MAILCOUNT_GMAIL_PASSWORD}" > "$tmp_wgetrc"
-		if shell_is_osx; then
-			mail=$(wget -q -O - https://mail.google.com/a/${TMUX_POWERLINE_SEG_MAILCOUNT_GMAIL_SERVER}/feed/atom --config "$tmp_wgetrc" | grep -E -m 1 -o '<fullcount>(.*)</fullcount>' | sed -e 's,.*<fullcount>\([^<]*\)</fullcount>.*,\1,g')
-		else
-			mail=$(wget -q -O - https://mail.google.com/a/${TMUX_POWERLINE_SEG_MAILCOUNT_GMAIL_SERVER}/feed/atom --config "$tmp_wgetrc" | grep fullcount | sed 's/<[^0-9]*>//g')
-		fi
+		mail=$(wget -q -O - https://mail.google.com/a/${TMUX_POWERLINE_SEG_MAILCOUNT_GMAIL_SERVER}/feed/atom --config "$tmp_wgetrc" | grep -E -m 1 -o '<fullcount>(.*)</fullcount>' | sed -e 's,.*<fullcount>\([^<]*\)</fullcount>.*,\1,g')
 		rm "$tmp_wgetrc"
 
 		if [ "$mail" != "" ]; then
