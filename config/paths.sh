28511eaba33545b51e297b52d9023b6e451fe397
Hagai Cohen <hagaic@mellanox.com>
2015-11-24 11:33:34 +0200

    Added the option to control RCFILE trough env
    Signed-off-by: Hagai Cohen <hagaic@mellanox.com>

--- config/paths.sh
+++ config/paths.sh
@@ -4,7 +4,9 @@ export TMUX_POWERLINE_DIR_LIB="$TMUX_POWERLINE_DIR_HOME/lib"
 export TMUX_POWERLINE_DIR_SEGMENTS="$TMUX_POWERLINE_DIR_HOME/segments"
 export TMUX_POWERLINE_DIR_TEMPORARY="/tmp/tmux-powerline_${USER}"
 export TMUX_POWERLINE_DIR_THEMES="$TMUX_POWERLINE_DIR_HOME/themes"
-export TMUX_POWERLINE_RCFILE="$HOME/.tmux-powerlinerc"
+if [ -z "$TMUX_POWERLINE_RCFILE" ]; then
+	export TMUX_POWERLINE_RCFILE="$HOME/.tmux-powerlinerc"
+fi
 export TMUX_POWERLINE_RCFILE_DEFAULT="$HOME/.tmux-powerlinerc.default"
 
 if [ ! -d "$TMUX_POWERLINE_DIR_TEMPORARY" ]; then
