60822ac4191c15f93a65bf462aeaa9f9884474c0
HuangChao <huangchao.cpp@gmail.com>
2016-03-09 19:04:11 +0800

    use papne_current_path, no need to set TMUXPWD

--- lib/tmux_adapter.sh
+++ lib/tmux_adapter.sh
@@ -1,7 +1,7 @@
 # Get the current path in the segment.
 get_tmux_cwd() {
 	local env_name=$(tmux display -p "#D" | tr -d %)
-	# use papne_current_path, no need to set TMUXPWD
+	# use pane_current_path, no need to set TMUXPWD
 	local env_val=$(tmux display-message -p -F "#{pane_current_path}" -t"$env_name")
 	# The version below is still quite new for tmux. Uncomment this in the future :-)
 	#local env_val=$(tmux show-environment "$env_name" 2>&1)
