743324084cef0f08ab04299f36b2ef90413aeca6
HuangChao <huangchao.cpp@gmail.com>
2016-03-09 18:56:37 +0800

    use new api to get_tmux_cwd

--- lib/tmux_adapter.sh
+++ lib/tmux_adapter.sh
@@ -1,7 +1,8 @@
 # Get the current path in the segment.
 get_tmux_cwd() {
-	local env_name=$(tmux display -p "TMUXPWD_#D" | tr -d %)
-	local env_val=$(tmux show-environment | grep --color=never "$env_name")
+	local env_name=$(tmux display -p "#D" | tr -d %)
+	#local env_val=$(tmux show-environment | grep --color=never "$env_name")
+  local env_val=$(tmux display-message -p -F "#{pane_current_path}" -t"$env_name")
 	# The version below is still quite new for tmux. Uncomment this in the future :-)
 	#local env_val=$(tmux show-environment "$env_name" 2>&1)
 
