04fa0df91748445bad0e43c50b679f1bd1b18a01
flytreeleft <flytreeleft@126.com>
2016-07-30 20:04:16 +0800

    fixed width of print out

--- segments/ifstat.sh
+++ segments/ifstat.sh
@@ -27,7 +27,7 @@ run_segment() {
 				;;
 		esac
 		if [ -n "${type}" ]; then
-			format=$(echo "${format} ${type} ⇊ %.2f ⇈ %.2f")
+			format=$(echo "${format} ${type} ⇊ %5.01f ⇈ %5.01f")
 			holder=$(echo "${holder},\$$((index)),\$$((index+1))")
 		fi
 		index=$((index+2))
