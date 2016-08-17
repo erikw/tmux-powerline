d44b7eee7f2815bf6edd71ea156d521a082521e2
Boris Aranovich <nomaed@users.noreply.github.com>
2016-08-18 00:31:16 +0300

    Indentation: 4 spaces => 1 tab

--- lib/formatting.sh
+++ lib/formatting.sh
@@ -1,7 +1,7 @@
 __print_colored_content() {
-    [ "${2:0:1}" = "#" ] && BGCOLOR="$2" || BGCOLOR="colour$2"
-    [ "${3:0:1}" = "#" ] && FGCOLOR="$3" || FGCOLOR="colour$3"
-    echo -n "#[fg=${FGCOLOR},bg=${BGCOLOR}]"
-    echo -n "$1"
-    echo -n "#[default]"
+	[ "${2:0:1}" = "#" ] && BGCOLOR="$2" || BGCOLOR="colour$2"
+	[ "${3:0:1}" = "#" ] && FGCOLOR="$3" || FGCOLOR="colour$3"
+	echo -n "#[fg=${FGCOLOR},bg=${BGCOLOR}]"
+	echo -n "$1"
+	echo -n "#[default]"
 }
