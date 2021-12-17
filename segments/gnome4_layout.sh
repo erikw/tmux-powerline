22a793592cd5475615025dac89a62fe6cc2b2c7b
SemyonDunaev <89798910+SemyonDunaev@users.noreply.github.com>
2021-12-17 18:01:23 +0200

    Show current keyboard layout for Gnome40

---
+++ segments/gnome4_layout.sh
@@ -0,0 +1,13 @@
+# Print the currently used keyboard layout
+# Works with Gnome 40
+# Set smaller update interval in .tmux.conf: 'set-option -g status-interval 1'
+# Exit if platform is not linux as this script is dependant on Linux and Gnome DE
+
+run_segment() {
+	if ! shell_is_linux; then
+		return 1
+	fi
+
+cur_layout=$(gsettings get org.gnome.desktop.input-sources mru-sources | awk -F"'" '{print $4}')
+	echo "⌨  $cur_layout"
+}
