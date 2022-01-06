aa43d2e2a89ae28b8c1197b1317a4ce44f221ce9
Erik Westrup <erik.westrup@gmail.com>
2022-01-06 12:43:44 +0100

    Slimmed down default theme a bit

--- themes/default.sh
+++ themes/default.sh
@@ -59,10 +59,10 @@ if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 		"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
 		"wan_ip 24 255" \
 		"vcs_branch 29 88" \
-		"vcs_compare 60 255" \
-		"vcs_staged 64 255" \
-		"vcs_modified 9 255" \
-		"vcs_others 245 0" \
+		#"vcs_compare 60 255" \
+		#"vcs_staged 64 255" \
+		#"vcs_modified 9 255" \
+		#"vcs_others 245 0" \
 	)
 fi
 
@@ -71,7 +71,7 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 		#"earthquake 3 0" \
 		"pwd 89 211" \
 		#"macos_notification_count 29 255" \
-		"mailcount 9 255" \
+		#"mailcount 9 255" \
 		"now_playing 234 37" \
 		#"cpu 240 136" \
 		"load 237 167" \
