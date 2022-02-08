dada6968f1a4d3af7b918cde3321e196ebbefbea
Erik Westrup <erik.westrup@gmail.com>
2022-02-08 09:13:07 +0100

    Fix lan_ip.sh

--- segments/lan_ip.sh
+++ segments/lan_ip.sh
@@ -2,8 +2,9 @@
 
 run_segment() {
 	if shell_is_bsd || shell_is_osx ; then
-		all_nics=$(ifconfig 2>/dev/null | awk -F':' '/^[a-z]/ && !/^lo/ { print $1 }')
-		for nic in ${all_nics[@]}; do
+		all_nics=$(ifconfig 2>/dev/null | awk -F':' '/^[a-z]/ && !/^lo/ { print $1 }' | tr '\n' ' ')
+		IFS=' ' read -ra all_nics_array <<< "$all_nics"
+		for nic in "${all_nics_array[@]}"; do
 			ipv4s_on_nic=$(ifconfig ${nic} 2>/dev/null | awk '$1 == "inet" { print $2 }')
 			for lan_ip in ${ipv4s_on_nic[@]}; do
 				[[ -n "${lan_ip}" ]] && break
