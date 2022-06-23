9ce48ceafda081986e11b047b86f940035043909
Erik Westrup <erik.westrup@gmail.com>
2022-06-23 14:20:27 +0200

    Refactor + supress stderr and stdout on WSL check

--- segments/now_playing.sh
+++ segments/now_playing.sh
@@ -283,22 +283,23 @@ __np_playerctl() {
 }
 
 __np_spotify() {
-    not_wsl = $(! which tasklist.exe )
-	if shell_is_linux && not_wsl; then
-		metadata=$(dbus-send --reply-timeout=42 --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' 2>/dev/null)
-		if [ "$?" -eq 0 ] && [ -n "$metadata" ]; then
-			# TODO how do one express this with dbus-send? It works with qdbus but the problem is that it's probably not as common as dbus-send.
-			state=$(qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get org.mpris.MediaPlayer2.Player PlaybackStatus)
-			if [[ $state == "Playing" ]]; then
-				artist=$(echo "$metadata" | grep -PA2 "string\s\"xesam:artist\"" | tail -1 | grep -Po "(?<=\").*(?=\")")
-				track=$(echo "$metadata" | grep -PA1 "string\s\"xesam:title\"" | tail -1 | grep -Po "(?<=\").*(?=\")")
-				np=$(echo "${artist} - ${track}")
+	if shell_is_linux; then
+		if type tasklist.exe >/dev/null 2>&1; then # WSL
+			np=$(tasklist.exe /APPS /fo list /v /fi "IMAGENAME eq Spotify.exe" | grep " - "  | cut -d" " -f3-)
+		else
+			metadata=$(dbus-send --reply-timeout=42 --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' 2>/dev/null)
+			if [ "$?" -eq 0 ] && [ -n "$metadata" ]; then
+				# TODO how do one express this with dbus-send? It works with qdbus but the problem is that it's probably not as common as dbus-send.
+				state=$(qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get org.mpris.MediaPlayer2.Player PlaybackStatus)
+				if [[ $state == "Playing" ]]; then
+					artist=$(echo "$metadata" | grep -PA2 "string\s\"xesam:artist\"" | tail -1 | grep -Po "(?<=\").*(?=\")")
+					track=$(echo "$metadata" | grep -PA1 "string\s\"xesam:title\"" | tail -1 | grep -Po "(?<=\").*(?=\")")
+					np=$(echo "${artist} - ${track}")
+				fi
 			fi
 		fi
 	elif shell_is_osx; then
 		np=$(${TMUX_POWERLINE_DIR_SEGMENTS}/np_spotify_mac.script)
-    else 
-        np=$(tasklist.exe /APPS /fo list  /v /fi "IMAGENAME eq Spotify.exe" | grep " - "  | cut -d" " -f3-)
     fi
     echo "${np//[$'\t\r\n']}"
 }
