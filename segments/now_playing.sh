e895b8516c10247a5546d5161e0ffbddaa4140cc
itaysaadia <itay.1234567891000@gmail.com>
2022-07-13 18:59:58 +0300

    Added support for windows (over WSL) for NP (spotify) (#288)
    * Added support for windows (over WSL) for NP (spotify)
    
    parsed tasklist for getting the now playing to work
    
    * Refactor + supress stderr and stdout on WSL check
    
    * Move linefeed stripping back in to WSL section
    
    * Fix mistake
    
    Co-authored-by: Erik Westrup <erik.westrup@gmail.com>

--- segments/now_playing.sh
+++ segments/now_playing.sh
@@ -284,14 +284,19 @@ __np_playerctl() {
 
 __np_spotify() {
 	if shell_is_linux; then
-		metadata=$(dbus-send --reply-timeout=42 --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' 2>/dev/null)
-		if [ "$?" -eq 0 ] && [ -n "$metadata" ]; then
-			# TODO how do one express this with dbus-send? It works with qdbus but the problem is that it's probably not as common as dbus-send.
-			state=$(qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get org.mpris.MediaPlayer2.Player PlaybackStatus)
-			if [[ $state == "Playing" ]]; then
-				artist=$(echo "$metadata" | grep -PA2 "string\s\"xesam:artist\"" | tail -1 | grep -Po "(?<=\").*(?=\")")
-				track=$(echo "$metadata" | grep -PA1 "string\s\"xesam:title\"" | tail -1 | grep -Po "(?<=\").*(?=\")")
-				np=$(echo "${artist} - ${track}")
+		if type tasklist.exe >/dev/null 2>&1; then # WSL
+			np=$(tasklist.exe /APPS /fo list /v /fi "IMAGENAME eq Spotify.exe" | grep " - "  | cut -d" " -f3-)
+			np="${np//[$'\t\r\n']}"
+		else
+			metadata=$(dbus-send --reply-timeout=42 --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' 2>/dev/null)
+			if [ "$?" -eq 0 ] && [ -n "$metadata" ]; then
+				# TODO how do one express this with dbus-send? It works with qdbus but the problem is that it's probably not as common as dbus-send.
+				state=$(qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get org.mpris.MediaPlayer2.Player PlaybackStatus)
+				if [[ $state == "Playing" ]]; then
+					artist=$(echo "$metadata" | grep -PA2 "string\s\"xesam:artist\"" | tail -1 | grep -Po "(?<=\").*(?=\")")
+					track=$(echo "$metadata" | grep -PA1 "string\s\"xesam:title\"" | tail -1 | grep -Po "(?<=\").*(?=\")")
+					np=$(echo "${artist} - ${track}")
+				fi
 			fi
 		fi
 	elif shell_is_osx; then
