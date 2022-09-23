887ed21108ec34614f2eaf4a93009c6cc4117ca2
Erik Westrup <erik.westrup@gmail.com>
2022-09-23 19:23:40 +0200

    Shell implementation of realpath
    as macOS doesn't ship with a realpath program.
    
    Fixes issue from #281

--- generate_rc.sh
+++ generate_rc.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 # Generate default rc file.
 
-export TMUX_POWERLINE_DIR_HOME="$(dirname $0)"
+# macOS doesn't ship with realpath.
+# Bash implementation from https://stackoverflow.com/a/18443300/265508
+realpath() (
+  OURPWD=$PWD
+  cd "$(dirname "$1")"
+  LINK=$(readlink "$(basename "$1")")
+  while [ "$LINK" ]; do
+    cd "$(dirname "$LINK")"
+    LINK=$(readlink "$(basename "$1")")
+  done
+  REALPATH="$PWD/$(basename "$1")"
+  cd "$OURPWD"
+  echo "$REALPATH"
+)
+
+export TMUX_POWERLINE_DIR_HOME="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+
 source "${TMUX_POWERLINE_DIR_HOME}/config/paths.sh"
 source "${TMUX_POWERLINE_DIR_HOME}/config/defaults.sh"
 source "${TMUX_POWERLINE_DIR_HOME}/config/shell.sh"
--- powerline.sh
+++ powerline.sh
@@ -2,6 +2,24 @@
 
 export TMUX_POWERLINE_DIR_HOME="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
+# macOS doesn't ship with realpath.
+# Bash implementation from https://stackoverflow.com/a/18443300/265508
+realpath() (
+  OURPWD=$PWD
+  cd "$(dirname "$1")"
+  LINK=$(readlink "$(basename "$1")")
+  while [ "$LINK" ]; do
+    cd "$(dirname "$LINK")"
+    LINK=$(readlink "$(basename "$1")")
+  done
+  REALPATH="$PWD/$(basename "$1")"
+  cd "$OURPWD"
+  echo "$REALPATH"
+)
+
+export TMUX_POWERLINE_DIR_HOME="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+
+
 source "${TMUX_POWERLINE_DIR_HOME}/config/helpers.sh"
 source "${TMUX_POWERLINE_DIR_HOME}/config/paths.sh"
 source "${TMUX_POWERLINE_DIR_HOME}/config/shell.sh"
