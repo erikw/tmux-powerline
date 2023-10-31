d80a931b205c7fa6f56d6b2173984d8296ed9c63
Danfeng Shan <shandanf@gmail.com>
2023-10-31 10:28:06 +0800

    Select a better lan ip with multiple nics for macos

--- segments/lan_ip.sh
+++ segments/lan_ip.sh
@@ -2,8 +2,11 @@
 
 run_segment() {
 	if shell_is_bsd || shell_is_osx ; then
+		default_route_nic=$(route get default | grep -i interface | awk '{print $2}')
 		all_nics=$(ifconfig 2>/dev/null | awk -F':' '/^[a-z]/ && !/^lo/ { print $1 }' | tr '\n' ' ')
 		IFS=' ' read -ra all_nics_array <<< "$all_nics"
+		# the nic of the default route is considered first
+		all_nics_array=("$default_route_nic" "${all_nics_array[@]}")
 		for nic in "${all_nics_array[@]}"; do
 			ipv4s_on_nic=$(ifconfig ${nic} 2>/dev/null | awk '$1 == "inet" { print $2 }')
 			for lan_ip in ${ipv4s_on_nic[@]}; do
