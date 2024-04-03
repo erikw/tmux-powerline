6c7d8c8d42bc74017fc155ba9ecc4444cae01b5f
Fabian Sylvester <xx4h@xx4h.de>
2024-04-03 19:14:16 +0000

    Remove double quotes, as behaviour differs between (#386)
    bash-3.2 (macOS) and newer bash (linux)
    
    fix #385
    
    Additionally:
    * move from mode numbers to names, for better readability
    * read path with "-r" to ensure path with "\" will be shown
      correctly
    * re-arrange mode selection so "name" mode is default, even
      when misconfigured

--- segments/vcs_rootpath.sh
+++ segments/vcs_rootpath.sh
@@ -1,12 +1,13 @@
+# shellcheck shell=bash
 # Source lib to get the function __get_vcs_root_path_and_vcs
 source "${TMUX_POWERLINE_DIR_LIB}/vcs_helper.sh"
 
-TMUX_POWERLINE_SEG_VCS_ROOTPATH_MODE="${TMUX_POWERLINE_SEG_VCS_ROOTPATH_MODE:-1}"
+TMUX_POWERLINE_SEG_VCS_ROOTPATH_MODE="${TMUX_POWERLINE_SEG_VCS_ROOTPATH_MODE:-name}"
 
 generate_segmentrc() {
 	read -d '' rccontents << EORC
 # Display mode for vcs_rootpath.
-# Example: (1: folder name only; 2: full path, w/o expansion; 3: full path, w/ tilde expansion)
+# Example: (name: folder name only; path: full path, w/o expansion; user_path: full path, w/ tilde expansion)
 # export TMUX_POWERLINE_SEG_VCS_ROOTPATH_MODE="${TMUX_POWERLINE_SEG_VCS_ROOTPATH_MODE}"
 EORC
 	echo "$rccontents"
@@ -16,17 +17,19 @@ EORC
 run_segment() {
 	__process_settings
 	# get root_path
-	{ read; read root_path; } <	<(get_vcs_type_and_root_path)
+	{ read; read -r root_path; } < <(get_vcs_type_and_root_path)
 
 	if [ -n "$root_path" ]; then
-		if [ "${TMUX_POWERLINE_SEG_VCS_ROOTPATH_MODE}" -eq 1 ]; then
-			root_path="${root_path/#$HOME/\~}"
-			root_path="${root_path/*\//}"
-		elif [ "${TMUX_POWERLINE_SEG_VCS_ROOTPATH_MODE}" -eq 2 ]; then
-			root_path="${root_path/#$HOME/\~}"
-		elif [ "${TMUX_POWERLINE_SEG_VCS_ROOTPATH_MODE}" -eq 3 ]; then
-			root_path="${root_path}"
-		fi
+		if [ "${TMUX_POWERLINE_SEG_VCS_ROOTPATH_MODE}" = "user_path" ]; then
+			root_path=${root_path/#$HOME/\~}
+		elif [ "${TMUX_POWERLINE_SEG_VCS_ROOTPATH_MODE}" = "path" ]; then
+			# no-op
+			true
+		# name, default, fallback for misconfiguration
+		elif [ "${TMUX_POWERLINE_SEG_VCS_ROOTPATH_MODE}" = "name" ] || :; then
+			root_path=${root_path/#$HOME/\~}
+			root_path=${root_path/*\//}
+    fi
 		echo -n "#[fg=${TMUX_POWERLINE_CUR_SEGMENT_FG}]${root_path}"
 	fi
 
