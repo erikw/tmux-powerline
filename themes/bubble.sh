85c910f1e595dd8997ec2a11e5396899b48a7cff
Brian Royer <brianroyer@gmail.com>
2024-04-08 17:08:14 -0400

    Fix tab issue

--- themes/bubble.sh
+++ themes/bubble.sh
@@ -127,38 +127,38 @@ fi
 
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
-    "tmux_session_info $blue $thm_bg" \
-    "hostname $eggplant $thm_bg" \
-    #"ifstat 30 255" \
-    #"ifstat_sys 30 255" \
-    "lan_ip $sky_blue $thm_bg ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
-    "wan_ip $sky_blue $thm_bg" \
-    "vcs_branch $thm_gray" \
-    #"air ${TMUX_POWERLINE_SEG_AIR_COLOR} $thm_bg" \
-    #"vcs_compare 60 255" \
-    #"vcs_staged 64 255" \
-    #"vcs_modified 9 255" \
-    #"vcs_others 245 0" \
+		"tmux_session_info $blue $thm_bg" \
+		"hostname $eggplant $thm_bg" \
+		#"ifstat 30 255" \
+		#"ifstat_sys 30 255" \
+		"lan_ip $sky_blue $thm_bg ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
+		"wan_ip $sky_blue $thm_bg" \
+		"vcs_branch $thm_gray" \
+		#"air ${TMUX_POWERLINE_SEG_AIR_COLOR} $thm_bg" \
+		#"vcs_compare 60 255" \
+		#"vcs_staged 64 255" \
+		#"vcs_modified 9 255" \
+		#"vcs_others 245 0" \
 	)
 fi
 
 if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
-    # "earthquake 3 0" \
-    "pwd $mauve $surface0" \
-    #"macos_notification_count 29 255" \
-    #"mailcount 9 255" \
-    "now_playing $spotify_green $spotify_black" \
-    #"cpu 240 136" \
-    #"load 237 167" \
-    #"tmux_mem_cpu_load 234 136" \
-    "battery $blue $thm_bg" \
-    #"weather 37 255" \
-    #"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}" \
-    #"xkb_layout 125 117" \
-    "date_day $teal $thm_bg" \
-    "date $teal $thm_bg ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-    "time $teal $thm_bg ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-    #"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		# "earthquake 3 0" \
+		"pwd $mauve $surface0" \
+		#"macos_notification_count 29 255" \
+		#"mailcount 9 255" \
+		"now_playing $spotify_green $spotify_black" \
+		#"cpu 240 136" \
+		#"load 237 167" \
+		#"tmux_mem_cpu_load 234 136" \
+		"battery $blue $thm_bg" \
+		#"weather 37 255" \
+		#"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}" \
+		#"xkb_layout 125 117" \
+		"date_day $teal $thm_bg" \
+		"date $teal $thm_bg ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		"time $teal $thm_bg ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		#"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 	)
 fi
--- themes/default.sh
+++ themes/default.sh
@@ -82,40 +82,40 @@ fi
 
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
-    "tmux_session_info 148 234" \
-    "hostname 33 0" \
-    #"mode_indicator 165 0" \
-    #"ifstat 30 255" \
-    #"ifstat_sys 30 255" \
-    "lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
-    #"vpn 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
-    "wan_ip 24 255" \
-    "vcs_branch 29 88" \
-    #"vcs_compare 60 255" \
-    #"vcs_staged 64 255" \
-    #"vcs_modified 9 255" \
-    #"vcs_others 245 0" \
+		"tmux_session_info 148 234" \
+		"hostname 33 0" \
+		#"mode_indicator 165 0" \
+		#"ifstat 30 255" \
+		#"ifstat_sys 30 255" \
+		"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
+		#"vpn 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
+		"wan_ip 24 255" \
+		"vcs_branch 29 88" \
+		#"vcs_compare 60 255" \
+		#"vcs_staged 64 255" \
+		#"vcs_modified 9 255" \
+		#"vcs_others 245 0" \
 	)
 fi
 
 if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
-    #"earthquake 3 0" \
-    "pwd 89 211" \
-    #"macos_notification_count 29 255" \
-    #"mailcount 9 255" \
-    "now_playing 234 37" \
-    #"cpu 240 136" \
-    "load 237 167" \
-    #"tmux_mem_cpu_load 234 136" \
-    "battery 137 127" \
-    #"air ${TMUX_POWERLINE_SEG_AIR_COLOR} 255" \
-    "weather 37 255" \
-    #"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}" \
-    #"xkb_layout 125 117" \
-    "date_day 235 136" \
-    "date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-    "time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-    #"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		#"earthquake 3 0" \
+		"pwd 89 211" \
+		#"macos_notification_count 29 255" \
+		#"mailcount 9 255" \
+		"now_playing 234 37" \
+		#"cpu 240 136" \
+		"load 237 167" \
+		#"tmux_mem_cpu_load 234 136" \
+		"battery 137 127" \
+		#"air ${TMUX_POWERLINE_SEG_AIR_COLOR} 255" \
+		"weather 37 255" \
+		#"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}" \
+		#"xkb_layout 125 117" \
+		"date_day 235 136" \
+		"date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		"time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		#"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 	)
 fi
