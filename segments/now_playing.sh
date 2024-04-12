d90c7549ffda44b100a3388d05cd5d301c5487ae
Fabian Sylvester <xx4h@xx4h.de>
2024-04-12 16:22:54 +0000

    Cleanup now_playing (#407)
    * comply with shellcheck
    * fix audacious output (current-song prints "artist - album - title",
      but we want only "artist - title")

--- segments/now_playing.sh
+++ segments/now_playing.sh
@@ -1,5 +1,7 @@
+# shellcheck shell=bash
 # Print current playing song in your music player of choice.
 
+# shellcheck source=lib/text_roll.sh
 source "${TMUX_POWERLINE_DIR_LIB}/text_roll.sh"
 
 TMUX_POWERLINE_SEG_NOW_PLAYING_MAX_LEN_DEFAULT="40"
@@ -18,7 +20,7 @@ TMUX_POWERLINE_SEG_NOW_PLAYING_NOTE_CHAR_DEFAULT="♫"
 TMUX_POWERLINE_SEG_NOW_PLAYING_MUSIC_PLAYER_DEFAULT="spotify"
 
 generate_segmentrc() {
-	read -d '' rccontents  << EORC
+	read -r -d '' rccontents  << EORC
 # Music player to use. Can be any of {audacious, banshee, cmus, apple_music, itunes, lastfm, plexamp, mocp, mpd, mpd_simple, pithos, playerctl, rdio, rhythmbox, spotify, spotify_wine, file}.
 export TMUX_POWERLINE_SEG_NOW_PLAYING_MUSIC_PLAYER="${TMUX_POWERLINE_SEG_NOW_PLAYING_MUSIC_PLAYER_DEFAULT}"
 # File to be read in case the song is being read from a file
@@ -119,7 +121,7 @@ run_segment() {
 	if [ -n "$np" ]; then
 		case "$TMUX_POWERLINE_SEG_NOW_PLAYING_TRIM_METHOD" in
 			"roll")
-				np=$(roll_text "${np}" ${TMUX_POWERLINE_SEG_NOW_PLAYING_MAX_LEN} ${TMUX_POWERLINE_SEG_NOW_PLAYING_ROLL_SPEED_DEFAULT} ${TMUX_POWERLINE_SEG_NOW_PLAYING_ROLL_MODE} "${TMUX_POWERLINE_SEG_NOW_PLAYING_ROLL_SEPARATOR}")
+				np=$(roll_text "${np}" "${TMUX_POWERLINE_SEG_NOW_PLAYING_MAX_LEN}" "${TMUX_POWERLINE_SEG_NOW_PLAYING_ROLL_SPEED_DEFAULT}" "${TMUX_POWERLINE_SEG_NOW_PLAYING_ROLL_MODE}" "${TMUX_POWERLINE_SEG_NOW_PLAYING_ROLL_SEPARATOR}")
 				;;
 			"trim")
 				np=${np:0:TMUX_POWERLINE_SEG_NOW_PLAYING_MAX_LEN}
@@ -170,7 +172,7 @@ __process_settings() {
 }
 
 __np_mpd() {
-	cd "$TMUX_POWERLINE_DIR_SEGMENTS"
+	cd "$TMUX_POWERLINE_DIR_SEGMENTS" || return
 
 	if [ ! -x "np_mpd" ]; then
 		make clean np_mpd &>/dev/null
@@ -185,17 +187,17 @@ __np_mpd() {
 }
 
 __np_file() {
-	np=$(cat $TMUX_POWERLINE_SEG_NOW_PLAYING_FILE_NAME | tr '\n' '|')
+	np=$(tr '\n' '|' < "$TMUX_POWERLINE_SEG_NOW_PLAYING_FILE_NAME")
 	echo "$np"
 }
 
 
 __np_mpd_simple() {
-	np=$(MPD_HOST="$TMUX_POWERLINE_SEG_NOW_PLAYING_MPD_HOST" MPD_PORT="$TMUX_POWERLINE_SEG_NOW_PLAYING_MPD_PORT" mpc current -f "$TMUX_POWERLINE_SEG_NOW_PLAYING_MPD_SIMPLE_FORMAT" 2>&1)
-	if [ $? -eq 0 ] && [ -n "$np" ]; then
-		mpc | grep "paused" > /dev/null
-		if [ $? -eq 0 ]; then
-			return 1
+	if np=$(MPD_HOST="$TMUX_POWERLINE_SEG_NOW_PLAYING_MPD_HOST" MPD_PORT="$TMUX_POWERLINE_SEG_NOW_PLAYING_MPD_PORT" mpc current -f "$TMUX_POWERLINE_SEG_NOW_PLAYING_MPD_SIMPLE_FORMAT" 2>&1); then
+		if [ -n "$np" ]; then
+			if mpc | grep "paused" > /dev/null; then
+				return 1
+			fi
 		fi
 		echo "$np"
 	fi
@@ -203,11 +205,10 @@ __np_mpd_simple() {
 
 
 __np_audacious() {
-	audacious_pid=$(pidof audacious)
-	if [ -n "$audacious_pid" ]; then
-		if $(audtool playback-playing); then
-			np=$(audtool current-song)
-			echo "$np"
+	if type audtool >/dev/null 2>&1; then
+		if audtool playback-playing; then
+			{ read -r artist; read -r title; } < <(audtool --current-song-tuple-data artist --current-song-tuple-data title)
+			echo "${artist} - ${title}"
 		fi
 	fi
 }
@@ -234,7 +235,7 @@ __np_cmus() {
 		#tracknumber=$(cmus-remote -Q | grep "tracknumber" | cut -d ' ' -f 3)
 		#album=$(cmus-remote -Q | grep "album" | cut -d ' ' -f 3-)
 
-		np=$(echo "${artist} - ${title}")
+		np="${artist} - ${title}"
 
 		if [ "$status" == "playing" ]; then
 			echo "$np"
@@ -243,14 +244,14 @@ __np_cmus() {
 }
 
 __np_apple_music() {
-	[ ! shell_is_osx ] && return 1
-	np=$(${TMUX_POWERLINE_DIR_SEGMENTS}/np_apple_music.script)
+	! shell_is_osx && return 1
+	np=$("${TMUX_POWERLINE_DIR_SEGMENTS}/np_apple_music.script")
 	echo "$np"
 }
 
 __np_itunes() {
-	[ ! shell_is_osx ] && return 1
-	np=$(${TMUX_POWERLINE_DIR_SEGMENTS}/np_itunes.script)
+	! shell_is_osx && return 1
+	np=$("${TMUX_POWERLINE_DIR_SEGMENTS}/np_itunes.script")
 	echo "$np"
 }
 
@@ -261,23 +262,26 @@ __np_lastfm() {
 
 	if [ -f "$TMP_FILE" ]; then
 		if shell_is_osx || shell_is_bsd; then
-			last_update=$(stat -f "%m" ${TMP_FILE})
+			last_update=$(stat -f "%m" "${TMP_FILE}")
 		elif shell_is_linux; then
-			last_update=$(stat -c "%Y" ${TMP_FILE})
+			last_update=$(stat -c "%Y" "${TMP_FILE}")
 		fi
 		time_now=$(date +%s)
 
 		up_to_date=$(echo "(${time_now}-${last_update}) < ${TMUX_POWERLINE_SEG_NOW_PLAYING_LASTFM_UPDATE_PERIOD}" | bc)
 		if [ "$up_to_date" -eq 1 ]; then
-			np=$(cat ${TMP_FILE})
+			np=$(cat "${TMP_FILE}")
 		fi
 	fi
 
 	if [ -z "$np" ]; then
-		local url=$(printf $ENDPOINT_FMT $TMUX_POWERLINE_SEG_NOW_PLAYING_LASTFM_USERNAME $TMUX_POWERLINE_SEG_NOW_PLAYING_LASTFM_API_KEY)
-		np=$(curl --silent "$url" | jq --raw-output '.recenttracks.track[0] | "\(.artist."#text") - \(.name)"')
-		if [ "$?" -eq "0" ] && [ -n "$np" ]; then
-			echo "${np}" > $TMP_FILE
+		local url
+		# shellcheck disable=SC2059
+		url=$(printf "$ENDPOINT_FMT" "$TMUX_POWERLINE_SEG_NOW_PLAYING_LASTFM_USERNAME" "$TMUX_POWERLINE_SEG_NOW_PLAYING_LASTFM_API_KEY")
+		if np=$(curl --silent "$url" | jq --raw-output '.recenttracks.track[0] | "\(.artist."#text") - \(.name)"'); then
+			if [ -n "$np" ]; then
+				echo "${np}" > "$TMP_FILE"
+			fi
 		fi
 	fi
 	echo "$np"
@@ -289,30 +293,33 @@ __np_plexamp() {
 
 	if [ -f "$TMP_FILE" ]; then
 		if shell_is_osx || shell_is_bsd; then
-			last_update=$(stat -f "%m" ${TMP_FILE})
+			last_update=$(stat -f "%m" "${TMP_FILE}")
 		elif shell_is_linux; then
-			last_update=$(stat -c "%Y" ${TMP_FILE})
+			last_update=$(stat -c "%Y" "${TMP_FILE}")
 		fi
 		time_now=$(date +%s)
 
 		up_to_date=$(echo "(${time_now}-${last_update}) < ${TMUX_POWERLINE_SEG_NOW_PLAYING_PLEXAMP_UPDATE_PERIOD}" | bc)
 		if [ "$up_to_date" -eq 1 ]; then
-			np=$(cat ${TMP_FILE})
+			np=$(cat "${TMP_FILE}")
 		fi
 	fi
 
 	if [ -z "$np" ]; then
-		local url=$(printf $ENDPOINT_FMT $TMUX_POWERLINE_SEG_NOW_PLAYING_PLEXAMP_TAUTULLI_HOST $TMUX_POWERLINE_SEG_NOW_PLAYING_PLEXAMP_TAUTULLI_API_KEY)
-		np=$(curl --silent "$url" | jq -r ".response.data.sessions[] | select(.username==\"$TMUX_POWERLINE_SEG_NOW_PLAYING_PLEXAMP_USERNAME\" and .media_type==\"track\" and .state==\"playing\") | .grandparent_title + \" - \" + .title")
-		if [ "$?" -eq "0" ] && [ -n "$np" ]; then
-			echo "${np}" > $TMP_FILE
+		local url
+		# shellcheck disable=SC2059
+		url=$(printf "$ENDPOINT_FMT" "$TMUX_POWERLINE_SEG_NOW_PLAYING_PLEXAMP_TAUTULLI_HOST" "$TMUX_POWERLINE_SEG_NOW_PLAYING_PLEXAMP_TAUTULLI_API_KEY")
+		if np=$(curl --silent "$url" | jq -r ".response.data.sessions[] | select(.username==\"$TMUX_POWERLINE_SEG_NOW_PLAYING_PLEXAMP_USERNAME\" and .media_type==\"track\" and .state==\"playing\") | .grandparent_title + \" - \" + .title"); then
+			if [ -n "$np" ]; then
+				echo "${np}" > "$TMP_FILE"
+			fi
 		fi
 	fi
 	echo "$np"
 }
 __np_pithos() {
 	if [ "$(dbus-send --reply-timeout=10 --print-reply --dest=net.kevinmehall.Pithos /net/kevinmehall/Pithos net.kevinmehall.Pithos.IsPlaying 2>/dev/null | grep boolean | cut -d' ' -f5)" == "true" ]; then
-		np=$(${TMUX_POWERLINE_DIR_SEGMENTS}/np_pithos.py)
+		np=$("${TMUX_POWERLINE_DIR_SEGMENTS}/np_pithos.py")
 		echo "$np"
 	fi
 }
@@ -322,15 +329,15 @@ __np_mocp() {
 	if [ -n "$mocp_pid" ]; then
 		np=$(mocp -i | grep ^Title | sed "s/^Title://")
 		mocp_paused=$(mocp -i | grep ^State | sed "s/^State: //")
-		if [ -n "$np" -a "$mocp_paused" != "PAUSE" ]; then
+		if [ -n "$np" ] && [ "$mocp_paused" != "PAUSE" ]; then
 			echo "$np"
 		fi
 	fi
 }
 
 __np_rdio() {
-	[ ! shell_is_osx ] && return 1
-	np=$(osascript ${TMUX_POWERLINE_DIR_SEGMENTS}/np_rdio_mac.script)
+	! shell_is_osx && return 1
+	np=$(osascript "${TMUX_POWERLINE_DIR_SEGMENTS}/np_rdio_mac.script")
 	echo "$np"
 }
 
@@ -360,26 +367,27 @@ __np_spotify() {
 			np=$(tasklist.exe /fo list /v /fi "IMAGENAME eq Spotify.exe" | grep " - "  | cut -d" " -f3-)
 			np="${np//[$'\t\r\n']}"
 		else
-			metadata=$(dbus-send --reply-timeout=42 --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' 2>/dev/null)
-			if [ "$?" -eq 0 ] && [ -n "$metadata" ]; then
-				state=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus'|grep -E -A 1 "string"|cut -b 26-|cut -d '"' -f 1|grep -E -v ^$)
-				if [[ $state == "Playing" ]]; then
-					artist=$(echo "$metadata" | grep -PA2 "string\s\"xesam:artist\"" | tail -1 | grep -Po "(?<=\").*(?=\")")
-					track=$(echo "$metadata" | grep -PA1 "string\s\"xesam:title\"" | tail -1 | grep -Po "(?<=\").*(?=\")")
-					np=$(echo "${artist} - ${track}")
+			if metadata=$(dbus-send --reply-timeout=42 --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' 2>/dev/null); then
+				if [ -n "$metadata" ]; then
+					state=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus'|grep -E -A 1 "string"|cut -b 26-|cut -d '"' -f 1|grep -E -v ^$)
+					if [[ $state == "Playing" ]]; then
+						artist=$(echo "$metadata" | grep -PA2 "string\s\"xesam:artist\"" | tail -1 | grep -Po "(?<=\").*(?=\")")
+						track=$(echo "$metadata" | grep -PA1 "string\s\"xesam:title\"" | tail -1 | grep -Po "(?<=\").*(?=\")")
+						np="${artist} - ${track}"
+					fi
 				fi
 			fi
 		fi
 	elif shell_is_osx; then
-		np=$(${TMUX_POWERLINE_DIR_SEGMENTS}/np_spotify_mac.script)
+		np=$("${TMUX_POWERLINE_DIR_SEGMENTS}/np_spotify_mac.script")
 	fi
 	echo "$np"
 }
 
 __np_spotify_wine() {
-	[ ! shell_is_linux ] && return 1
+	! shell_is_linux && return 1
 	spotify_id=$(xwininfo -root -tree | grep '("spotify' | cut -f1 -d'"' | sed 's/ //g')
-	echo $spotify_id
+	echo "$spotify_id"
 	if [ -n "$spotify_id" ]; then
 		np=$(xwininfo -id "$spotify_id" | grep "xwininfo.*Spotify -" | grep -Po "(?<=\"Spotify - ).*(?=\"$)")
 		echo "$np"
