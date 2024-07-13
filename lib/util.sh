717688b1dd9ca3bf062b44a59fb2206e07e28418
Fabian Sylvester <xx4h@xx4h.de>
2024-07-13 14:33:07 +0200

    New segment github_notifications (#432)
    * add new util function "is_tmp_valid" to make handling
      of temp files based on last update reusable
    * add new segment
    * add jq requirement to README.md
    
    fixes #431

--- lib/util.sh
+++ lib/util.sh
@@ -12,3 +12,33 @@ is_flag_enabled() {
 		;;
 	esac
 }
+
+is_tmp_valid() {
+	local tmp_file="$1"
+	local tmp_validity="$2"
+
+	if [ -f "$tmp_file" ]; then
+		if shell_is_osx || shell_is_bsd; then
+			stat >/dev/null 2>&1 && is_gnu_stat=false || is_gnu_stat=true
+			if [ "$is_gnu_stat" == "true" ]; then
+				last_update=$(stat -c "%Y" "${tmp_file}")
+			else
+				last_update=$(stat -f "%m" "${tmp_file}")
+			fi
+		elif shell_is_linux || [ -z "$is_gnu_stat" ]; then
+			last_update=$(stat -c "%Y" "${tmp_file}")
+		fi
+
+		time_now=$(date +%s)
+		valid=$(echo "(${time_now}-${last_update}) < ${tmp_validity}" | bc)
+
+		if [ "$valid" -eq 1 ]; then
+			return 0
+		else
+			return 1
+		fi
+	else
+		return 1
+	fi
+
+}
