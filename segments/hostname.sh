b1e0f55bd49ce7cdf80553f0d2ba0c451836d672
Ryuuusuke <makewishmr@gmail.com>
2025-01-25 15:06:48 +0700

    Added support with hostnamectl

--- segments/hostname.sh
+++ segments/hostname.sh
@@ -3,24 +3,33 @@
 
 TMUX_POWERLINE_SEG_HOSTNAME_FORMAT="${TMUX_POWERLINE_SEG_HOSTNAME_FORMAT:-short}"
 
+# check if user has hostname command
+has_hostname() {
+  command -v $1 >/dev/null
+}
+
 generate_segmentrc() {
-	read -r -d '' rccontents <<EORC
+  read -r -d '' rccontents <<EORC
 # Use short or long format for the hostname. Can be {"short, long"}.
 export TMUX_POWERLINE_SEG_HOSTNAME_FORMAT="${TMUX_POWERLINE_SEG_HOSTNAME_FORMAT}"
 EORC
-	echo "$rccontents"
+  echo "$rccontents"
 }
 
 run_segment() {
-	local opts=""
-	if [ "$TMUX_POWERLINE_SEG_HOSTNAME_FORMAT" == "short" ]; then
-		if shell_is_osx || shell_is_bsd; then
-			opts="-s"
-		else
-			opts="--short"
-		fi
-	fi
+  local opts=""
+  if [ "$TMUX_POWERLINE_SEG_HOSTNAME_FORMAT" == "short" ]; then
+    if shell_is_osx || shell_is_bsd; then
+      opts="hostname"
+    else
+      opts="hostname"
+    fi
+  fi
 
-	hostname ${opts}
-	return 0
+  if has_hostname hostname; then
+    hostname ${opts}
+  else
+    hostnamectl hostname
+  fi
+  return 0
 }
