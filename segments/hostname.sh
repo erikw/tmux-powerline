8da200e603db96f570cd38ceab55de77f5bc5646
Ryusuke <62561046+Ryuuusuke@users.noreply.github.com>
2025-02-09 09:26:34 +0700

    Update hostname.sh
    Hi, i changed the name for the function like you said. Yes, it's better to use more general function name,I didn't check the indent before, now the indent is same as before, also this will check both hostname and hostnamectl now! :)p

--- segments/hostname.sh
+++ segments/hostname.sh
@@ -3,33 +3,35 @@
 
 TMUX_POWERLINE_SEG_HOSTNAME_FORMAT="${TMUX_POWERLINE_SEG_HOSTNAME_FORMAT:-short}"
 
-# check if user has hostname command
-has_hostname() {
-  command -v $1 >/dev/null
+command_exists() {
+	command -v $1 > /dev/null 
 }
 
 generate_segmentrc() {
-  read -r -d '' rccontents <<EORC
+	read -r -d '' rccontents <<EORC
 # Use short or long format for the hostname. Can be {"short, long"}.
 export TMUX_POWERLINE_SEG_HOSTNAME_FORMAT="${TMUX_POWERLINE_SEG_HOSTNAME_FORMAT}"
 EORC
-  echo "$rccontents"
+	echo "$rccontents"
 }
 
 run_segment() {
-  local opts=""
-  if [ "$TMUX_POWERLINE_SEG_HOSTNAME_FORMAT" == "short" ]; then
-    if shell_is_osx || shell_is_bsd; then
-      opts="-s"
-    else
-      opts="--short"
-    fi
-  fi
+	local opts=""
+	if [ "$TMUX_POWERLINE_SEG_HOSTNAME_FORMAT" == "short" ]; then
+		if shell_is_osx || shell_is_bsd; then
+			opts="-s"
+		else
+			opts="--short"
+		fi
+	fi
+	
+	if command_exists hostname; then
+		hostname ${opts}
+	elif command_exists hostnamectl; then
+		hostnamectl hostname
+	else
+		echo 'Host'
+	fi
+	return 0
 
-  if has_hostname hostname; then
-    hostname ${opts}
-  else
-    hostnamectl hostname
-  fi
-  return 0
 }
