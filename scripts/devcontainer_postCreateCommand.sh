ad02b8e358dfe87ddad03c83d371f17006b679cb
Erik Westrup <erik.westrup@icloud.com>
2025-03-03 11:07:36 +0000

    Fix linter errors

--- scripts/devcontainer_postCreateCommand.sh
+++ scripts/devcontainer_postCreateCommand.sh
@@ -4,21 +4,21 @@
 
 set -eux
 
-# Defensive sysop: in case user's dotfiles installed tmux configs already.
+# Defensive (aggressive?) sysop: in case user's dotfiles installed tmux configs already.
 ## Tmux
-rm -rf ${XDG_CONFIG_HOME:-$HOME/.config}/tmux
-rm -rf $HOME/.tmux
+rm -rf "${XDG_CONFIG_HOME:-$HOME/.config}"/tmux
+rm -rf "$HOME"/.tmux
 
 ## tmux-powerline
-rm -rf ${XDG_CONFIG_HOME:-$HOME/.config}/tmux-powerline
-rm -rf $HOME/.tmux-powerline
-rm -f $HOME/.tmuxpowerlinerc
+rm -rf "${XDG_CONFIG_HOME:-$HOME/.config}"/tmux-powerline
+rm -rf "$HOME"/.tmux-powerline
+rm -f "$HOME"/.tmuxpowerlinerc
 
 
 # Set up TPM. Ref: https://github.com/tmux-plugins/tpm?#installation
-git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm "$HOME"/.tmux/plugins/tpm
 
-cat << TMUXCONF > $HOME/.tmux.conf
+cat << TMUXCONF > "$HOME"/.tmux.conf
 # List of plugins
 set -g @plugin 'tmux-plugins/tpm'
 # Install tmux-powerline as a "local plugin". Ref: https://github.com/tmux-plugins/tpm/issues/220#issuecomment-1082686994
@@ -29,4 +29,4 @@ run '~/.tmux/plugins/tpm/tpm'
 TMUXCONF
 
 # Install TPM plugins
-$HOME/.tmux/plugins/tpm/bin/install_plugins
+"$HOME"/.tmux/plugins/tpm/bin/install_plugins
