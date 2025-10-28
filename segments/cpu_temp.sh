ef5c83ce19d8fc3f8f988cb6cabbc94621f88cf2
iyegoroff <iegoroff@gmail.com>
2025-10-28 15:44:33 +0300

    improve cpu_temp

--- segments/cpu_temp.sh
+++ segments/cpu_temp.sh
@@ -4,20 +4,25 @@
 #   lm_sensors (Linux only)
 #   smctemp (Macos only)
 
-TMUX_POWERLINE_SEG_CPU_TEMP_ICON="${TMUX_POWERLINE_SEG_CPU_TEMP_ICON:- }"
-TMUX_POWERLINE_SEG_CPU_TEMP_SENSORS_LINE_MARKER="${TMUX_POWERLINE_SEG_CPU_TEMP_SENSORS_LINE_MARKER:-Package id 0\|Physical id 0\|temp1}"
+TMUX_POWERLINE_SEG_CPU_TEMP_ICON_DEFAULT=" "
+TMUX_POWERLINE_SEG_CPU_TEMP_SENSORS_LINE_MARKER_DEFAULT="Package id 0\|Physical id 0\|temp1"
 
 generate_segmentrc() {
 	read -r -d '' rccontents <<EORC
 # CPU temperature icon
-export TMUX_POWERLINE_SEG_CPU_TEMP_ICON="${TMUX_POWERLINE_SEG_CPU_TEMP_ICON}"
-# Regexp to indicate a line containing CPU temperature in sensors output
-export TMUX_POWERLINE_SEG_CPU_TEMP_SENSORS_LINE_MARKER="${TMUX_POWERLINE_SEG_CPU_TEMP_SENSORS_LINE_MARKER}"
+export TMUX_POWERLINE_SEG_CPU_TEMP_ICON="${TMUX_POWERLINE_SEG_CPU_TEMP_ICON_DEFAULT}"
+# Linux only. Regexp to indicate a line containing CPU temperature in 'sensors' output.
+# Check the output of 'sensors' program, decide which line contains desired CPU temperature
+# and store an unique part of that line in this variable. It will be used by 'grep' program
+# to distinct the 'CPU temperature' line from the rest output lines.
+export TMUX_POWERLINE_SEG_CPU_TEMP_SENSORS_LINE_MARKER="${TMUX_POWERLINE_SEG_CPU_TEMP_SENSORS_LINE_MARKER_DEFAULT}"
 EORC
 	echo "$rccontents"
 }
 
 run_segment() {
+	__process_settings
+
 	local temp
 	temp=$(tp_cpu_temp_value)
 
@@ -29,3 +34,11 @@ run_segment() {
 	fi
 }
 
+__process_settings() {
+	if [ -z "$TMUX_POWERLINE_SEG_CPU_TEMP_ICON" ]; then
+		export TMUX_POWERLINE_SEG_CPU_TEMP_ICON="${TMUX_POWERLINE_SEG_CPU_TEMP_ICON_DEFAULT}"
+	fi
+	if [ -z "$TMUX_POWERLINE_SEG_CPU_TEMP_SENSORS_LINE_MARKER" ]; then
+		export TMUX_POWERLINE_SEG_CPU_TEMP_SENSORS_LINE_MARKER="${TMUX_POWERLINE_SEG_CPU_TEMP_SENSORS_LINE_MARKER_DEFAULT}"
+	fi
+}
