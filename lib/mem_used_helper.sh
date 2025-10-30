d4227f042931aae01c4ff07b5e3579953e46e9e3
iyegoroff <iegoroff@gmail.com>
2025-10-30 13:35:33 +0300

    fix lint error

--- lib/mem_used_helper.sh
+++ lib/mem_used_helper.sh
@@ -11,13 +11,13 @@ __tp_mem_used_info() {
 		local mem_used_bytes
 
 		stats=$(vm_stat)
-		bytes_per_page=$(echo $stats | sed -e 's/.*page size of \([0-9]*\).*/\1/')
+		bytes_per_page=$(echo "$stats" | sed -e 's/.*page size of \([0-9]*\).*/\1/')
 		mem_total_bytes=$(sysctl hw.memsize | sed -e 's/^hw.memsize: \([0-9*]\)/\1/')
-		free_pages=$(echo $stats | sed -e 's/.*Pages free: \([0-9]*\).*/\1/')
-		external_pages=$(echo $stats | sed -e 's/.*File-backed pages: \([0-9]*\).*/\1/')
+		free_pages=$(echo "$stats" | sed -e 's/.*Pages free: \([0-9]*\).*/\1/')
+		external_pages=$(echo "$stats" | sed -e 's/.*File-backed pages: \([0-9]*\).*/\1/')
 		mem_used_bytes=$(echo "$mem_total_bytes - ($free_pages + $external_pages) * $bytes_per_page" | bc -l)
 
-		echo $mem_used_bytes $mem_total_bytes
+		echo "$mem_used_bytes" "$mem_total_bytes"
 
 	elif tp_shell_is_linux; then
 		local meminfo
@@ -31,16 +31,16 @@ __tp_mem_used_info() {
 		local mem_used_bytes
 
 		meminfo=$(cat /proc/meminfo)
-		mem_total=$(echo $meminfo | sed -e 's/^MemTotal: \([0-9]*\).*/\1/')
+		mem_total=$(echo "$meminfo" | sed -e 's/^MemTotal: \([0-9]*\).*/\1/')
 		mem_total_bytes=$(echo "$mem_total * 1024" | bc -l)
-		mem_free=$(echo $meminfo | sed -e 's/.* MemFree: \([0-9]*\).*/\1/')
-		shmem=$(echo $meminfo | sed -e 's/.* Shmem: \([0-9]*\).*/\1/')
-		buffers=$(echo $meminfo | sed -e 's/.* Buffers: \([0-9]*\).*/\1/')
-		cached=$(echo $meminfo | sed -e 's/.* Cached: \([0-9]*\).*/\1/')
-		s_reclaimable=$(echo $meminfo | sed -e 's/.* SReclaimable: \([0-9]*\).*/\1/')
+		mem_free=$(echo "$meminfo" | sed -e 's/.* MemFree: \([0-9]*\).*/\1/')
+		shmem=$(echo "$meminfo" | sed -e 's/.* Shmem: \([0-9]*\).*/\1/')
+		buffers=$(echo "$meminfo" | sed -e 's/.* Buffers: \([0-9]*\).*/\1/')
+		cached=$(echo "$meminfo" | sed -e 's/.* Cached: \([0-9]*\).*/\1/')
+		s_reclaimable=$(echo "$meminfo" | sed -e 's/.* SReclaimable: \([0-9]*\).*/\1/')
 		mem_used_bytes=$(echo "($mem_total - $mem_free + $shmem - $buffers - $cached - $s_reclaimable) * 1024" | bc -l)
 
-		echo $mem_used_bytes $mem_total_bytes
+		echo "$mem_used_bytes" "$mem_total_bytes"
 	fi
 };
 
